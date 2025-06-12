8e6b40abc2dc05cc82d2b392f532b3d014942d4c
tmx <maplender@gmail.com>
2025-06-12 14:22:28 +0200

    Trying to get final version

--- Package.swift
+++ Package.swift
@@ -7,21 +7,30 @@ let package = Package(
         .iOS(.v15)
     ],
     products: [
-        .library(
-            name: "IdentitySDK",
-            targets: ["IdentitySDK"]
-        )
+       .library(
+           name: "IdentitySDK",
+           targets: ["IdentitySDKWrapper"]
+       )
     ],
     dependencies: [
-           .package(url: "https://github.com/signicat/videoidskd-spm.git", exact: "1.38.1")
-       ],
+        .package(url: "https://github.com/signicat/videoidskd-spm.git", exact: "1.38.1")
+    ],
     targets: [
-        /// The binary XCFramework target
-        .binaryTarget(
-            name: "IdentitySDK",
-            url: "https://github.com/signicat-indentity-api/identity-sdk-ios/archive/refs/tags/v3.2.2.zip",
-            checksum: "4e19a91853585038dc600a4f7a5d59cf4737bbc2bc97eee7590747cf4d4b0447"
-        )
+        .target(
+           name: "IdentitySDKWrapper",
+           dependencies: [
+               .target(name: "IdentitySDK"),
+               .product(name: "VideoID", package: "videoidskd-spm")
+           ],
+           path: "Sources/IdentitySDKWrapper"
+       ),
+
+       /// The binary XCFramework target
+       .binaryTarget(
+           name: "IdentitySDK",
+           url: "https://github.com/signicat-indentity-api/identity-sdk-ios/archive/refs/tags/v3.2.2.zip",
+           checksum: "4e19a91853585038dc600a4f7a5d59cf4737bbc2bc97eee7590747cf4d4b0447"
+       )
     ]
 )
 
